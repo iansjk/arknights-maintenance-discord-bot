d87c4cdea7d3a0578bbf3bc589d5263a2ab1790b
Ian Kim <iansjk@gmail.com>
2021-07-17 23:32:37 -0400

    Set up slash command demo

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,21 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: ['GUILD_MESSAGES', 'GUILD_INTEGRATIONS'] });
 client.login(process.env.TOKEN);
+
 client.once('ready', () => {
-  console.log('ready!');
+  const guild = client.guilds.cache.get(process.env.HOME_GUILD as any);
+  if (guild) {
+    guild.commands.create({ name: 'ping', description: 'Replies with "Pong!"' });
+  }
+});
+
+client.on('interactionCreate', (interaction) => {
+  if (interaction.isCommand()) {
+    console.log(interaction);
+    if (interaction.commandName === 'ping') {
+      interaction.reply('Pong!');
+    }
+  }
 });
